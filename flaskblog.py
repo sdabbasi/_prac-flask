64c8f193422c5766192876fe13153821cf16b573
sdabbasi <sd.abbasi75@gmail.com>
2022-06-23 00:28:56 +0430

    Move flask init notes to NOTE file

--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,3 @@
-# set a virtual environment
-# pip install virtualenv
-# pip install flask
-# to run the flask app, activate the prepared venv
-# set environment variable FLASK_APP:
-#   in powershell use: $env:FLASK_APP="flaskblog.py"
-#   in linux use: export FLASK_APP=flaskblog.py
-# also can set environment variable FLASK_DEBUG=1
-# then: flask run
-# --------
-# instead of the above method, can run the app as usual by using __name__ variable.
-
-
 from flask import Flask
 
 app = Flask(__name__)
