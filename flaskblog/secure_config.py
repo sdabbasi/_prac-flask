aa1095006b271a93b78f8215fcfd013f8624f236
sdabbasi <sd.abbasi75@gmail.com>
2022-07-05 19:52:30 +0430

    add secure_config file

---
+++ flaskblog/secure_config.py
@@ -0,0 +1,11 @@
+import os
+
+
+class Config():
+    SECRET_KEY = ''
+    SQLALCHEMY_DATABASE_URI = ''
+    MAIL_SERVER = ''
+    MAIL_PORT = ''
+    MAIL_USE_TLS = ''
+    MAIL_USERNAME = ''
+    MAIL_PASSWORD = ''
